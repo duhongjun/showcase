f86e1381fa7633d67f66520fab6833e14b5978d4
duhongjun <13651236533@163.com>
2019-06-25 22:38:54 +0800

    Updates

---
+++ precache-manifest.80ce31418fb187b818a0ba4bdb2c63b9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd8e1be9b3098f562c684b14bce96e9",
+    "url": "/showcase/index.html"
+  },
+  {
+    "revision": "3b68713ec3d765c6d236",
+    "url": "/showcase/static/css/main.f3312295.chunk.css"
+  },
+  {
+    "revision": "542712263057fc9aae39",
+    "url": "/showcase/static/js/2.fc7b1802.chunk.js"
+  },
+  {
+    "revision": "3b68713ec3d765c6d236",
+    "url": "/showcase/static/js/main.7eca9e76.chunk.js"
+  },
+  {
+    "revision": "56e2ab9d774d9b991a7f",
+    "url": "/showcase/static/js/runtime~main.ac16f234.js"
+  },
+  {
+    "revision": "7be06c0e4ddae32e11ecfd151d14706c",
+    "url": "/showcase/static/media/flap.7be06c0e.wav"
+  },
+  {
+    "revision": "d220163a310a49eec32dcc2b19075d92",
+    "url": "/showcase/static/media/flappyfont.d220163a.fnt"
+  },
+  {
+    "revision": "8183b3f6d3b8b09013eeaee277d29435",
+    "url": "/showcase/static/media/ouch.8183b3f6.wav"
+  },
+  {
+    "revision": "a5ab197768265e350f6244af4e373ea3",
+    "url": "/showcase/static/media/pipe-hit.a5ab1977.wav"
+  },
+  {
+    "revision": "57ca1a2085d82f0574e3ef740b9a5ead",
+    "url": "/showcase/static/media/preloader.57ca1a20.gif"
+  },
+  {
+    "revision": "dd35f4f19ca89920bbd5eecb91ae56d6",
+    "url": "/showcase/static/media/score.dd35f4f1.wav"
+  }
+]);
